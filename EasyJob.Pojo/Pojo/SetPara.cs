c03b47e1292d8512cb048e1ec9474c802064630c
linyuxiangfly <280789588@qq.com>
2016-01-19 17:16:59 +0800

    增加系统设置与用户设置Pojo

---
+++ EasyJob.Pojo/Pojo/SetPara.cs
@@ -0,0 +1,17 @@
+﻿using EasyJob.Pojo.Pojo.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyJob.Pojo.Pojo
+{
+    /// <summary>
+    /// 系统信息
+    /// </summary>
+    class SetPara : TbId
+    {
+        public virtual string ParaName { get; set; }
+        public virtual string ParaValue { get; set; }
+    }
+}
---
+++ EasyJob.Pojo/Pojo/UserSetPara.cs
@@ -0,0 +1,18 @@
+﻿using EasyJob.Pojo.Pojo.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyJob.Pojo.Pojo
+{
+    /// <summary>
+    /// 用户设置参数
+    /// </summary>
+    class UserSetPara : TbId
+    {
+        public virtual string ParaName { get; set; }
+        public virtual string ParaValue { get; set; }
+        public virtual Employee Oper { get; set; }
+    }
+}

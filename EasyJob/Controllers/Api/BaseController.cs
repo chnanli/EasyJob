7a2b452923ce9cb8bb10583f001ecd96e2ef04d7
linyuxiangfly <280789588@qq.com>
2016-01-26 15:00:28 +0800

    格式化JSON时间类型

--- EasyJob/Controllers/Api/BaseController.cs
+++ EasyJob/Controllers/Api/BaseController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using EasyJob.Pojo.Pojo;
 using ORM.Hibernate;
+using EasyJob.Result;
 
 namespace EasyJob.Controllers.Api
 {
@@ -28,6 +29,57 @@ namespace EasyJob.Controllers.Api
         public string GetUrl()
         {
             return "http://"+Request.Url.Host + Request.Url.PathAndQuery;
+        }
+
+
+        /// <summary>
+        /// 返回JsonResult
+        /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="contentType">内容类型</param>
+        /// <param name="contentEncoding">内容编码</param>
+        /// <param name="behavior">行为</param>
+        /// <returns>JsonReuslt</returns>
+        protected override JsonResult Json(object data, string contentType, System.Text.Encoding contentEncoding, JsonRequestBehavior behavior)
+        {
+            return new CustomJsonResult
+            {
+                Data = data,
+                ContentType = contentType,
+                ContentEncoding = contentEncoding,
+                JsonRequestBehavior = behavior,
+                FormateStr = "yyyy-MM-dd HH:mm:ss"
+            };
+        }
+
+        /// <summary>
+        /// 返回JsonResult.24         /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="behavior">行为</param>
+        /// <param name="format">json中dateTime类型的格式</param>
+        /// <returns>Json</returns>
+        protected JsonResult Json(object data, JsonRequestBehavior behavior, string format)
+        {
+            return new CustomJsonResult
+            {
+                Data = data,
+                JsonRequestBehavior = behavior,
+                FormateStr = format
+            };
+        }
+
+        /// <summary>
+        /// 返回JsonResult42         /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="format">数据格式</param>
+        /// <returns>Json</returns>
+        protected JsonResult Json(object data, string format)
+        {
+            return new CustomJsonResult
+            {
+                Data = data,
+                FormateStr = format
+            };
         }
     }
 }
